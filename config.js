173279eb428351c53e90b708f55ab3ce2fc8dc82
Mateusz Kalaga <46076250+mateuszkdev@users.noreply.github.com>
2020-03-26 11:36:43 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	prefix: "ab!",
 	owners: ["Mateusz"],
-	token: "NTQzNzc4Nzg1MTQxNzE5MDQx.Xjm3aw.wIitAtZ9ZOg3DuLqwTApDNSaV5Q",
+	token: "super sekretny token bota",
 	kolor: "#152323",
 	ownerid: "395266229436153868",
 	bladpermissji: "Błąd: Nie masz uprawnień do użycia tej komendy! ",
